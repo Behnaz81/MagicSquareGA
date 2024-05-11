82020cb853cc2b7ca87d0d779db1aaea464fe1ca
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-05-11 06:06:32 +0330

    Changed for n = 9

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 ###Project Title: MagicSquareGA
 ###Author: Behnaz Mohammad Hasani Zadeh
 ###Date: 4/28/2024
-###Github Repository: https://github.com/Behnaz81/MagicSquareGA
 ###Description: This project will solve Magic Square problem
 ###using genetic algorithm.
 ############################################################
@@ -11,10 +10,69 @@ import random
 import matplotlib.pyplot as plt
 
 ##The N##
-SQUARE_SIZE = 3
+SQUARE_SIZE = 9
+
+##Primaty Population Size##
+PRIMARY_POPULATION_SIZE = 1000
 
 ##The Mutation Rate##
-MUTATION_RATE = 0.05
+MUTATION_RATE = 0.03
+
+##The Cross Over Rate##
+CROSSOVER_RATE = 0.8
+
+##The T Tournament Selection##
+T = 2
+
+
+#######################Print#######################
+
+######This function helps us to print the genes.
+def printElement(element):
+    for i in range(SQUARE_SIZE):
+        for j in range(SQUARE_SIZE):
+            print(element[i * 9 + j], end=" ")
+        print()
+
+
+
+####################Fitness#######################
+
+######This function calculates the fitness of a gene.
+######It's based on how many rows, columns and diameters
+######don't have the sum we want. So the less the fitness is,
+######the better the gene is.
+def fitness(element):
+
+
+    error = 0
+
+    #For rows
+    for j in range(SQUARE_SIZE):
+        sum = 0
+        for i in range(SQUARE_SIZE):
+            sum += element[j * 9 + i]
+        if sum != SQUARE_SIZE * (SQUARE_SIZE ** 2 + 1) / 2:
+            error += 1
+
+    #For columns
+    for j in range(SQUARE_SIZE):
+        sum = 0
+        for i in range(SQUARE_SIZE):
+            sum += element[j + i * 9]
+        if sum != SQUARE_SIZE * (SQUARE_SIZE ** 2 + 1) / 2:
+            error += 1
+
+    #For diameter
+    if (element[0] + element[10] + element[19] + element[30]+ element[40] + element[50] + element[60] + element[70] + element[80]) != SQUARE_SIZE * (SQUARE_SIZE ** 2 + 1) / 2:
+        error += 1
+
+    if (element[8] + element[16] + element[24] + element[32] + element[40] + element[48] + element[56] + element[63] + element[72]) != SQUARE_SIZE * (SQUARE_SIZE ** 2 + 1) / 2:
+        error += 1
+
+    return error
+
+
 
 ##############Make The Primary Population#############
 
@@ -29,44 +87,52 @@ def makePrimaryPopulation(populationSize):
 
 
 
-###################Make the Secondary Population#################
+###############################Selection############################
 
 ######This function uses TOURNAMENT selection method to choose
 ######from the primary population.
-def makeSecondaryPopulation(primaryPopulation, populationSize):
+def selection(primaryPopulation, populationSize):
     secondaryPopulation = []
-    for i in range(populationSize):
-        element1 = random.randint(0, len(primaryPopulation) - 1)
-        element2 = random.randint(0, len(primaryPopulation) - 1)
+    secondaryFitnesses = []
 
-        while element1 == element2:
-            element2 = random.randint(0, len(primaryPopulation) - 1)
+    while len(secondaryPopulation) < populationSize:
+        elements = []
+        fitnesses = []
 
-        if fitness(primaryPopulation[element1]) > fitness(primaryPopulation[element2]):
-            secondaryPopulation.append(population[element1])
-        else:
-            secondaryPopulation.append(population[element2])
+        for i in range(T):
+            randomNumber = random.randint(0, len(primaryPopulation) - 1)
 
-    return secondaryPopulation
+            while primaryPopulation[randomNumber] in elements:
+                randomNumber = random.randint(0, len(primaryPopulation) - 1)
 
+            elements.append(primaryPopulation[randomNumber])
+            fitnesses.append(fitness(primaryPopulation[randomNumber]))
+
+        if elements[fitnesses.index(min(fitnesses))] not in secondaryPopulation:
+            secondaryPopulation.append(elements[fitnesses.index(min(fitnesses))])
+            secondaryFitnesses.append(min(fitnesses))
+
+    return secondaryPopulation, secondaryFitnesses
 
 
 #################Crossover#############################
 
 ######This function helps us to make new genes
-######To pretend repeating elements we used this method:
-######The first 3 elements come from one parent
-######The rest are from the elements we haven't used from
-######the other parent
-def crossover(secondaryPopulation):
+######First a random point is chosen.
+######Then the first part of the child will be
+######from the one parent. Other elements are
+######from the non-repeated elements from the
+######other parent.
+def crossover(parent1, parent2):
     child1 = []
     child2 = []
-    parent1 = secondaryPopulation[0]
-    parent2 = secondaryPopulation[1]
-    for i in range(SQUARE_SIZE):
+    randomPoint = random.randint(0, len(parent1) - 2)
+
+    for i in range(randomPoint):
         child1.append(parent1[i])
         child2.append(parent2[i])
-    for i in range(SQUARE_SIZE * 3):
+
+    for i in range(SQUARE_SIZE ** 2):
         if parent2[i] not in child1:
             child1.append(parent2[i])
         if parent1[i] not in child2:
@@ -78,70 +144,22 @@ def crossover(secondaryPopulation):
 
 ########################Mutation#########################
 
-######This function will do the mutation. For each element
+######This function will do the mutation. For the element
 ######a random number between 0 and 1 will be generated. If
-######the random number was less than mutation rate the element
-######swaps places with another random element.
-def mutation(population):
-    fitnesses = []
-    for element in population:
-        for i in range(0, len(element)):
+######the random number was less than mutation rate two
+######random elements will swap places.
+def mutation(element):
 
-            randomNumber = random.random()
+    randomNumber = random.random()
 
-            if randomNumber < MUTATION_RATE:
-                indx = random.randint(0, len(element) - 1)
-                while indx == i:
-                    indx = random.randint(0, len(element) - 1)
-                element[i], element[indx] = element[indx], element[i]
-        fitnesses.append(fitness(element))
+    if randomNumber < MUTATION_RATE:
+        element1 = random.randint(0, len(element) - 1)
+        element2 = random.randint(0, len(element) - 1)
+        while element1 == element2:
+           element2 = random.randint(0, len(element) - 1)
+        element[element1], element[element2] = element[element2], element[element1]
 
-    return population, fitnesses
-
-
-####################Fitness#######################
-
-######This function calculates the fitness of a gene.
-######It's based on how many rows, columns and diameters
-######don't have the sum of 15. So the less the fitness is,
-######the better the gene is.
-def fitness(element):
-    error = 0
-
-    #For rows
-    for j in range(SQUARE_SIZE):
-        sum = 0
-        for i in range(SQUARE_SIZE):
-            sum += element[j * 3 + i]
-        if sum != 15:
-            error += 1
-
-    #For columns
-    for j in range(SQUARE_SIZE):
-        sum = 0
-        for i in range(SQUARE_SIZE):
-            sum += element[j + i * 3]
-        if sum != 15:
-            error += 1
-
-    #For diameter
-    if (element[0] + element[4] + element[8]) != 15:
-        error += 1
-
-    if (element[3] + element[5] + element[7]) != 15:
-        error += 1
-
-    return error
-
-
-
-#######################Print#######################
-######This function helps us to print the genes.
-def printElement(element):
-    for i in range(SQUARE_SIZE):
-        for j in range(SQUARE_SIZE):
-            print(element[i * 3 + j], end="")
-        print()
+    return element
 
 
 
@@ -149,60 +167,65 @@ def printElement(element):
 
 fitnesses = []
 bestFitnesses = []
-#Make a population with 50 genes
-population = makePrimaryPopulation(50)
+
+#Make a primary population
+population = makePrimaryPopulation(PRIMARY_POPULATION_SIZE)
+
 #Save the fitnesses in a list
-for i in range(50):
+for i in range(PRIMARY_POPULATION_SIZE):
     fitnesses.append(fitness(population[i]))
 
-# print("Primary population:")
-#Printing the first population
-# for element in population:
-#     printElement(element)
-#     print()
-
 bestFitnesses.append(min(fitnesses))
+generation = 0
+
+print(generation, bestFitnesses[generation])
 
 #As long as the minimum fitness is 2 or more we continue making new genes
-while(min(fitnesses) >= 2):
+while(min(fitnesses) > 2):
+    fitnesses = []
 
-    #Choose two parents and make a secondary population
-    secondaryPopulation = makeSecondaryPopulation(population, 2)
+    #Choose parents and make a secondary population
+    secondaryPopulation, secondaryFitnesses = selection(population, int(PRIMARY_POPULATION_SIZE * CROSSOVER_RATE))
 
-    # print("Secondary population:")
-    # for element in secondaryPopulation:
-    #     printElement(element)
-    #     print()
+    #Do the cross over for each two element from the secondary population
+    length = len(secondaryPopulation)
+    for i in range(0, length, 2):
+        child1, child2 = crossover(secondaryPopulation[i], secondaryPopulation[i + 1])
+        if child1 not in secondaryPopulation:
+            secondaryPopulation.append(child1)
+            secondaryFitnesses.append(fitness(child1))
+        if child2 not in secondaryPopulation:
+            secondaryPopulation.append(child2)
+            secondaryFitnesses.append(fitness(child2))
 
-    #Do the cross over
-    child1, child2 = crossover(secondaryPopulation)
+    #Do the mutation for each element of the secondary population
+    for i in range(len(secondaryPopulation)):
+        afterMutation = mutation(secondaryPopulation[i])
+        if afterMutation != secondaryPopulation[i]:
+            secondaryPopulation.append(afterMutation)
+            secondaryFitnesses.append(fitness(afterMutation))
 
-    # print("After Crossover:")
-    # printElement(child1)
-    # print()
-    # printElement(child2)
-    # print()
+    #Add the secondary population to the primary population
+    for i in range(len(secondaryPopulation)):
+        if secondaryPopulation[i] not in population:
+            population.append(secondaryPopulation[i])
+            fitnesses.append(secondaryFitnesses[i])
 
-    #Add the new genes to the population
-    population.append(child1)
-    population.append(child2)
-
-    #Mutate the whole population
-    population, fitnesses = mutation(population)
+    #Choose from the new population using selection function
+    population, fitnesses = selection(population, PRIMARY_POPULATION_SIZE)
 
     #Find the best fitness of this generation
     bestFitnesses.append(min(fitnesses))
-    
-    # print("After Mutation:")
-    # for element in secondaryPopulation:
-    #     printElement(element)
-    #     print()
-
+    generation += 1
+    print(generation, bestFitnesses[generation], len(population))
 
 #The final solution!
 print("Final Solution:")
 printElement(population[fitnesses.index(min(fitnesses))])
 
 #To show the best fitnesses we had in each generation
-plt.scatter(list(range(len(bestFitnesses))), bestFitnesses)
+plt.plot(list(range(len(bestFitnesses))), bestFitnesses)
+plt.xlabel("Generations")
+plt.ylabel("Best Fitness")
+plt.title("Fitness vs Generations")
 plt.show()
